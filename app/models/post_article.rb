e6f5adae9429c83aff4f8207237afab6975caabb
Chie035 <bs5.spf10@gmail.com>
2021-08-02 14:45:23 +0000

    [Add]新規登録機能・管理者ログイン

---
+++ app/models/post_article.rb
@@ -0,0 +1,4 @@
+class PostArticle < ApplicationRecord
+  
+  belongs_to :user
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :adimin do
+  end
+  
+    devise_for :admins, controllers: {
+      sessions: 'admins/sessions'
+    }
+
+
+  scope module: :public do
+    devise_for :users
+    
+    root to: "homes#top"
+    get "/about" => "homes#about", as: "about"
+  end
+
 end
