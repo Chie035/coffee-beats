77d700c5aa4a95ac26d167a4a5976669a303a48d
Chie035 <bs5.spf10@gmail.com>
2021-08-03 15:04:55 +0000

    [Add]Admins/contoroller usres,articles,post_articles,questions

---
+++ app/helpers/admins/post_articles_helper.rb
@@ -0,0 +1,2 @@
+module Admins::PostArticlesHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
 
   namespace :admins do
     get "/top" => "homes#top", as: "top"
+    
+    resources :articles
+    resources :users, only:[:index, :show, :edit, :update]
+    resources :post_articles, only:[:index, :show, :destroy]
+    resources :questions, only:[:index, :create, :edit, :update, :destroy]
   end
   
     devise_for :admins, controllers: {
